d9c69e6608f708917f2c43ca87c07b4e55e1eb28
Adam Brzeski <30234642+ambrzeski@users.noreply.github.com>
2019-10-05 11:59:07 +0200

    Basepath replaced with config; Resolving csv paths from rsna19.datasets by default

--- rsna19/data/dataset.py
+++ rsna19/data/dataset.py
@@ -5,23 +5,27 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
+from rsna19.config import config
+
 
 class IntracranialDataset(Dataset):
 
-    def __init__(self, csv_file, basepath, return_labels, folds, preprocess_func=None):
+    def __init__(self, csv_file, folds, csv_root_dir=None, return_labels=True, preprocess_func=None):
         """
         :param csv_file: path to csv file
-        :param basepath: root dataset path, prepended to paths from csv file
-        :param return_labels: if True, labels will be returned with image
         :param folds: list of selected folds
+        :param csv_root_dir: prepended to csv_file path, defaults to project's rsna19/datasets
+        :param return_labels: if True, labels will be returned with image
         :param preprocess_func: preprocessing function, e.g. for window adjustment
         """
 
-        self.basepath = basepath
         self.return_labels = return_labels
         self.preprocess_func = preprocess_func
 
-        data = pd.read_csv(csv_file)
+        if csv_root_dir is None:
+            csv_root_dir = os.path.normpath(__file__ + '/../../datasets')
+
+        data = pd.read_csv(os.path.join(csv_root_dir, csv_file))
         data = data[data.fold.isin(folds)]
         data = data.reset_index()
         self.data = data
@@ -31,7 +35,7 @@ class IntracranialDataset(Dataset):
 
     def __getitem__(self, idx):
 
-        img_path = os.path.join(self.basepath, self.data.loc[idx, 'path'])
+        img_path = os.path.normpath(os.path.join(config.data_root, '..', self.data.loc[idx, 'path']))
         img = np.load(img_path)
 
         if self.preprocess_func:
