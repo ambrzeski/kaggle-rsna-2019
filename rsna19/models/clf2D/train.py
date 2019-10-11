de27c2c215d3317d5004ce422b5313cc726289d1
Dmytro Poplavskiy <dmytro.poplavskiy@gmail.com>
2019-10-12 00:08:21 +1000

    log metrics during training and save oof per sample stats

--- rsna19/models/clf2D/train.py
+++ rsna19/models/clf2D/train.py
@@ -14,11 +14,13 @@ import albumentations
 import albumentations.pytorch
 import cv2
 import matplotlib.pyplot as plt
+import sklearn.metrics
 
 import torch.nn as nn
 import torch.nn.functional as F
 from rsna19 import config
 from rsna19.models.commons import radam
+from rsna19.models.commons import metrics
 from rsna19.models.clf2D.experiments import MODELS
 from torch.utils.tensorboard import SummaryWriter
 
@@ -28,6 +30,26 @@ def build_model_str(model_name, fold, run):
     fold_str = '' if fold == -1 else f'_fold_{fold}'
 
     return f'{model_name}{run_str}{fold_str}'
+
+
+def log_metrics(logger, phase, epoch_num, y_hat, y):
+    th = 0.5
+    accuracy = metrics.accuracy(y_hat, y, th, True)
+    f1_score = metrics.f1score(y_hat, y, th, True)
+    specificity = metrics.specificity(y_hat, y, th, True)
+    sensitivity = metrics.sensitivity(y_hat, y, th, True)
+    roc_auc = metrics.roc_auc(y_hat, y)
+
+    classes = ['epidural', 'intraparenchymal', 'intraventricular', 'subarachnoid', 'subdural', 'any']
+    for acc, f1, spec, sens, roc, class_name in zip(accuracy, f1_score, specificity, sensitivity, roc_auc, classes):
+        logger.add_scalar(f'{phase}_acc_{class_name}', acc, epoch_num)
+        logger.add_scalar(f'{phase}_f1_{class_name}', f1, epoch_num)
+        logger.add_scalar(f'{phase}_spec_{class_name}', spec, epoch_num)
+        logger.add_scalar(f'{phase}_sens_{class_name}', sens, epoch_num)
+        logger.add_scalar(f'{phase}_roc_{class_name}', roc, epoch_num)
+
+    for i, class_name in enumerate(classes):
+        logger.add_scalar(f'{phase}_bce_{class_name}', sklearn.metrics.log_loss(y[:, i], y_hat[:, i]), epoch_num)
 
 
 def train(model_name, fold, run=None, resume_epoch=-1):
@@ -65,7 +87,7 @@ def train(model_name, fold, run=None, resume_epoch=-1):
             albumentations.ShiftScaleRotate(shift_limit=16./256, scale_limit=0.1, rotate_limit=30,
                                             interpolation=cv2.INTER_LINEAR,
                                             border_mode=cv2.BORDER_REPLICATE,
-                                            p=0.75),
+                                            p=0.80),
             albumentations.HorizontalFlip(),
             albumentations.pytorch.ToTensorV2()
         ]),
@@ -82,7 +104,9 @@ def train(model_name, fold, run=None, resume_epoch=-1):
     model.train()
     optimizer = radam.RAdam(model.parameters(), lr=model_info.initial_lr)
 
-    milestones = [4, 6, 10]
+    milestones = [5, 10, 16]
+    if model_info.optimiser_milestones:
+        milestones = model_info.optimiser_milestones
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=0.2)
 
     print(f'Num training images: {len(dataset_train)} validation images: {len(dataset_valid)}')
@@ -102,14 +126,19 @@ def train(model_name, fold, run=None, resume_epoch=-1):
                             batch_size=model_info.batch_size)
     }
 
-    criterium = nn.BCEWithLogitsLoss()
+    class_weights = torch.tensor([1.0, 1.0, 1.0, 1.0, 1.0, 2.0]).cuda()
+
+    def criterium(y_pred,y_true):
+        return F.binary_cross_entropy_with_logits(y_pred, y_true, class_weights.repeat(y_pred.shape[0], 1))
+
+    # criterium = nn.BCEWithLogitsLoss()
 
     # fit new layers first:
     if resume_epoch == -1 and model_info.is_pretrained:
         model.train()
         model.module.freeze_encoder()
         data_loader = data_loaders['train']
-        pre_fit_steps = 50000 // model_info.batch_size
+        pre_fit_steps = 100000 // model_info.batch_size
         data_iter = tqdm(enumerate(data_loader), total=pre_fit_steps)
         epoch_loss = []
         initial_optimizer = radam.RAdam(model.parameters(), lr=1e-3)
@@ -134,6 +163,9 @@ def train(model_name, fold, run=None, resume_epoch=-1):
         for phase in ['train', 'val']:
             model.train(phase == 'train')
             epoch_loss = []
+            epoch_labels = []
+            epoch_predictions = []
+            epoch_sample_paths = []
 
             data_loader = data_loaders[phase]
             data_iter = tqdm(enumerate(data_loader), total=len(data_loader))
@@ -154,11 +186,19 @@ def train(model_name, fold, run=None, resume_epoch=-1):
 
                     epoch_loss.append(float(loss))
 
+                    epoch_labels.append(labels.detach().cpu().numpy())
+                    epoch_predictions.append(torch.sigmoid(pred).detach().cpu().numpy())
+                    epoch_sample_paths += data['path']
+
                 data_iter.set_description(
                     f'{epoch_num} Loss: Running {np.mean(epoch_loss[-1000:]):1.4f} Avg {np.mean(epoch_loss):1.4f}')
 
+            epoch_labels = np.row_stack(epoch_labels)
+            epoch_predictions = np.row_stack(epoch_predictions)
+
             logger.add_scalar(f'loss_{phase}', np.mean(epoch_loss), epoch_num)
             logger.add_scalar('lr', optimizer.param_groups[0]['lr'], epoch_num)  # scheduler.get_lr()[0]
+            log_metrics(logger=logger, phase=phase, epoch_num=epoch_num, y=epoch_labels, y_hat=epoch_predictions)
             logger.flush()
 
             if phase == 'val':
@@ -172,6 +212,16 @@ def train(model_name, fold, run=None, resume_epoch=-1):
                         'optimizer_state_dict': optimizer.state_dict(),
                     },
                     f'{checkpoints_dir}/{epoch_num:03}.pt'
+                )
+
+                torch.save(
+                    {
+                        'epoch': epoch_num,
+                        'sample_paths': epoch_sample_paths,
+                        'epoch_labels': epoch_labels,
+                        'epoch_predictions': epoch_predictions,
+                    },
+                    f'{oof_dir}/{epoch_num:03}.pt'
                 )
 
 
@@ -231,6 +281,61 @@ def check_heatmap(model_name, fold, epoch, run=None):
             plt.show()
 
 
+def check_windows(model_name, fold, epoch, run=None):
+    model_str = build_model_str(model_name, fold, run)
+    model_info = MODELS[model_name]
+
+    checkpoints_dir = f'{config.config.checkpoints_dir}/{model_str}'
+    print('\n', model_name, '\n')
+
+    model = model_info.factory(**model_info.args)
+    model = model.cpu()
+
+    dataset_valid = dataset.IntracranialDataset(
+        csv_file='5fold.csv',
+        folds=[fold],
+        preprocess_func=albumentations.pytorch.ToTensorV2(),
+        **model_info.dataset_args
+    )
+
+    model.eval()
+    checkpoint = torch.load(f'{checkpoints_dir}/{epoch:03}.pt')
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model = model.cpu()
+
+    w = model.windows_conv.weight.detach().cpu().numpy().flatten()
+    b = model.windows_conv.bias.detach().cpu().numpy()
+    print(w, b)
+    for wi, bi in zip(w, b):
+        print(f'{-int(bi/wi*1000)} +- {int(abs(1000/wi))}')
+
+    batch_size = 1
+
+    data_loader = DataLoader(dataset_valid,
+                          shuffle=False,
+                          num_workers=16,
+                          batch_size=batch_size)
+
+    data_iter = tqdm(enumerate(data_loader), total=len(data_loader))
+    for iter_num, data in data_iter:
+        img = data['image'].float().cpu()
+        labels = data['labels'].detach().numpy()
+
+        with torch.set_grad_enabled(False):
+            windowed_img = model.windows_conv(img)
+            windowed_img = F.relu6(windowed_img).cpu().numpy()
+
+            fig, ax = plt.subplots(4, 4)
+
+            for batch in range(batch_size):
+                print(labels[batch], data['path'][batch])
+                for j in range(4):
+                    for k in range(4):
+                        ax[j, k].imshow(windowed_img[batch, j*4+k], cmap='gray')
+
+            plt.show()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('action', type=str, default='check')
@@ -254,3 +359,6 @@ if __name__ == '__main__':
 
     if action == 'check_heatmap':
         check_heatmap(model_name=args.model, run=args.run, fold=args.fold, epoch=args.epoch)
+
+    if action == 'check_windows':
+        check_windows(model_name=args.model, run=args.run, fold=args.fold, epoch=args.epoch)
