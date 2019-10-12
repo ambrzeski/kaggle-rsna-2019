2734f1fe42543a194e2f05c2faac29281698dc70
kha-white <kha-white@mail.com>
2019-10-12 19:02:13 +0200

    fix missing fields in config

--- rsna19/configs/se_resnext50_2dc.py
+++ rsna19/configs/se_resnext50_2dc.py
@@ -2,11 +2,11 @@ from rsna19.configs.base_config import BaseConfig
 
 
 class Config(BaseConfig):
-    train_out_dir = '/kolos/m2/ct/models/classification/rsna/0002_balancing_sampler/1_2_3_4'
+    train_out_dir = '/kolos/m2/ct/models/classification/rsna/0005_cdf/0_1_2_4'
 
     dataset_file = '5fold.csv'
-    train_folds = [1, 2, 3, 4]
-    val_folds = [0]
+    train_folds = [0, 1, 2, 4]
+    val_folds = [3]
 
     backbone = 'se_resnext50'
     pretrained = True
@@ -14,19 +14,21 @@ class Config(BaseConfig):
     lr = 0.0001
     batch_size = 64  # 16 (3, 512, 512) images fits on TITAN XP
 
-    gpus = [0]
+    gpus = [2]
     num_workers = 3 * len(gpus)
 
     max_epoch = 20
 
     num_slices = 3  # must be odd
     slice_size = 256
-    use_cdf = False
+    use_cdf = True
+    augment = True
 
     # used only if use_cdf is False
     min_hu_value = -1000
     max_hu_value = 1000
 
+    balancing = False
     # 'epidural', 'intraparenchymal', 'intraventricular', 'subarachnoid', 'subdural', no_bleeding
     probas = [0.1, 0.14, 0.14, 0.14, 0.14, 0.34]
 
