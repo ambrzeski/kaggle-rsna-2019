3eba05d6dda6b80ff47f595d353fc3e2ca0093aa
Maciej Budyś <kha-white@mail.com>
2019-10-15 15:40:07 +0200

    flat_anneal scheduling (#14)

--- rsna19/configs/se_resnext50_2dc.py
+++ rsna19/configs/se_resnext50_2dc.py
@@ -2,22 +2,29 @@ from rsna19.configs.base_config import BaseConfig
 
 
 class Config(BaseConfig):
-    train_out_dir = '/kolos/m2/ct/models/classification/rsna/0009_regularization/0_1_2_4'
+    train_out_dir = '/kolos/m2/ct/models/classification/rsna/0012_flat_anneal_4/0_1_2_3'
 
     dataset_file = '5fold.csv'
-    train_folds = [0, 1, 2, 4]
-    val_folds = [3]
+    train_folds = [0, 1, 2, 3]
+    val_folds = [4]
 
     backbone = 'se_resnext50'
     pretrained = True
 
-    lr = 0.0001
+    lr = 1e-4
     batch_size = 64  # 16 (3, 512, 512) images fits on TITAN XP
     dropout = 0.5
-    weight_decay = 0.01
+    weight_decay = 0.001
     optimizer = 'radam'
 
-    gpus = [2]
+    scheduler = {
+        'name': 'flat_anneal',
+        'flat_iterations': 8000,
+        'anneal_iterations': 17000,
+        'min_lr': 1e-7
+    }
+
+    gpus = [1]
     num_workers = 3 * len(gpus)
 
     max_epoch = 20
@@ -34,4 +41,3 @@ class Config(BaseConfig):
     balancing = False
     # 'epidural', 'intraparenchymal', 'intraventricular', 'subarachnoid', 'subdural', no_bleeding
     probas = [0.1, 0.14, 0.14, 0.14, 0.14, 0.34]
-
