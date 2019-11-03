55b18361e20cffbd9712fd304bf222f1622cbed7
Dmytro Poplavskiy <dmytro.poplavskiy@gmail.com>
2019-11-03 18:23:53 +1000

    added Apex support for training

--- rsna19/models/clf2D/train.py
+++ rsna19/models/clf2D/train.py
@@ -15,6 +15,7 @@ import albumentations.pytorch
 import cv2
 import matplotlib.pyplot as plt
 import sklearn.metrics
+from apex import amp
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -52,7 +53,7 @@ def log_metrics(logger, phase, epoch_num, y_hat, y):
         logger.add_scalar(f'{phase}_bce_{class_name}', sklearn.metrics.log_loss(y[:, i], y_hat[:, i]), epoch_num)
 
 
-def train(model_name, fold, run=None, resume_epoch=-1):
+def train(model_name, fold, run=None, resume_epoch=-1, use_apex=False):
     model_str = build_model_str(model_name, fold, run)
 
     model_info = MODELS[model_name]
@@ -77,7 +78,7 @@ def train(model_name, fold, run=None, resume_epoch=-1):
     #     raise
     #     pass
 
-    model = torch.nn.DataParallel(model).cuda()
+    # model = torch.nn.DataParallel(model).cuda()
     model = model.cuda()
 
     augmentations = [
@@ -164,37 +165,22 @@ def train(model_name, fold, run=None, resume_epoch=-1):
             batch_size=model_info.batch_size*2)
 
     model.train()
-    optimizer = radam.RAdam(model.parameters(), lr=model_info.initial_lr)
-
-    milestones = [5, 10, 16]
-    if model_info.optimiser_milestones:
-        milestones = model_info.optimiser_milestones
-    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=0.2)
-
-    print(f'Num training images: {len(dataset_train)} validation images: {len(dataset_valid)}')
-
-    if resume_epoch > -1:
-        checkpoint = torch.load(f'{checkpoints_dir}/{resume_epoch:03}.pt')
-        print('load', f'{checkpoints_dir}/{resume_epoch:03}.pt')
-        model.module.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
     class_weights = torch.tensor([1.0, 1.0, 1.0, 1.0, 1.0, 2.0]).cuda()
 
     def criterium(y_pred,y_true):
         return F.binary_cross_entropy_with_logits(y_pred, y_true, class_weights.repeat(y_pred.shape[0], 1))
 
-    # criterium = nn.BCEWithLogitsLoss()
-
-    # fit new layers first:
+    # fit the new layers first:
     if resume_epoch == -1 and model_info.is_pretrained:
         model.train()
-        model.module.freeze_encoder()
+        model.freeze_encoder()
         data_loader = data_loaders.get('train_1_slice', data_loaders['train'])
-        pre_fit_steps = 50000 // model_info.batch_size
+        pre_fit_steps = 40000 // model_info.batch_size
         data_iter = tqdm(enumerate(data_loader), total=pre_fit_steps)
         epoch_loss = []
-        initial_optimizer = radam.RAdam(model.parameters(), lr=1e-4)
+        initial_optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
         for iter_num, data in data_iter:
             if iter_num > pre_fit_steps:
                 break
@@ -210,7 +196,26 @@ def train(model_name, fold, run=None, resume_epoch=-1):
                 epoch_loss.append(float(loss))
 
                 data_iter.set_description(f'Loss: Running {np.mean(epoch_loss[-500:]):1.4f} Avg {np.mean(epoch_loss):1.4f}')
-    model.module.unfreeze_encoder()
+        model.unfreeze_encoder()
+
+    optimizer = radam.RAdam(model.parameters(), lr=model_info.initial_lr)
+    if use_apex:
+        model, optimizer = amp.initialize(model, optimizer, opt_level='O2')
+
+    milestones = [5, 10, 16]
+    if model_info.optimiser_milestones:
+        milestones = model_info.optimiser_milestones
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=0.2)
+
+    print(f'Num training images: {len(dataset_train)} validation images: {len(dataset_valid)}')
+
+    if resume_epoch > -1:
+        checkpoint = torch.load(f'{checkpoints_dir}/{resume_epoch:03}.pt')
+        print('load', f'{checkpoints_dir}/{resume_epoch:03}.pt')
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        if 'amp' in checkpoint:
+            amp.load_state_dict(checkpoint['amp'])
 
     for epoch_num in range(resume_epoch+1, 12):
         for phase in ['train', 'val']:
@@ -220,8 +225,8 @@ def train(model_name, fold, run=None, resume_epoch=-1):
             epoch_predictions = []
             epoch_sample_paths = []
 
-            if 'on_epoch' in model.module.__dir__():
-                model.module.on_epoch(epoch_num)
+            if 'on_epoch' in model.__dir__():
+                model.on_epoch(epoch_num)
 
             if epoch_num < model_info.single_slice_steps:
                 data_loader = data_loaders[phase+'_1_slice']
@@ -242,7 +247,7 @@ def train(model_name, fold, run=None, resume_epoch=-1):
             #     print("freeze bn")
             #     model.module.freeze_bn()
 
-            data_iter = tqdm(enumerate(data_loader), total=len(data_loader))
+            data_iter = tqdm(enumerate(data_loader), total=len(data_loader), ncols=200)
             for iter_num, data in data_iter:
                 img = data['image'].float().cuda()
                 labels = data['labels'].float().cuda()
@@ -258,9 +263,15 @@ def train(model_name, fold, run=None, resume_epoch=-1):
                     loss = criterium(pred, labels)
 
                     if phase == 'train':
-                        (loss / model_info.accumulation_steps).backward()
+                        if use_apex:
+                            with amp.scale_loss(loss / model_info.accumulation_steps, optimizer) as scaled_loss:
+                                scaled_loss.backward()
+                        else:
+                            (loss / model_info.accumulation_steps).backward()
+
                         if (iter_num + 1) % model_info.accumulation_steps == 0:
-                            torch.nn.utils.clip_grad_norm_(model.parameters(), 32.0)
+                            # if not use_apex:
+                            #     torch.nn.utils.clip_grad_norm_(model.parameters(), 32.0)
                             optimizer.step()
                             optimizer.zero_grad()
 
@@ -273,12 +284,12 @@ def train(model_name, fold, run=None, resume_epoch=-1):
                 data_iter.set_description(
                     f'{epoch_num} Loss: Running {np.mean(epoch_loss[-1000:]):1.4f} Avg {np.mean(epoch_loss):1.4f}')
 
-            epoch_labels = np.row_stack(epoch_labels)
-            epoch_predictions = np.row_stack(epoch_predictions)
-
             logger.add_scalar(f'loss_{phase}', np.mean(epoch_loss), epoch_num)
             logger.add_scalar('lr', optimizer.param_groups[0]['lr'], epoch_num)  # scheduler.get_lr()[0]
             try:
+                epoch_labels = np.row_stack(epoch_labels)
+                epoch_predictions = np.row_stack(epoch_predictions)
+                print(epoch_labels.shape, epoch_predictions.shape)
                 log_metrics(logger=logger, phase=phase, epoch_num=epoch_num, y=epoch_labels, y_hat=epoch_predictions)
             except Exception:
                 pass
@@ -300,8 +311,9 @@ def train(model_name, fold, run=None, resume_epoch=-1):
                 torch.save(
                     {
                         'epoch': epoch_num,
-                        'model_state_dict': model.module.state_dict(),
+                        'model_state_dict': model.state_dict(),
                         'optimizer_state_dict': optimizer.state_dict(),
+                        'amp': amp.state_dict()
                     },
                     f'{checkpoints_dir}/{epoch_num:03}.pt'
                 )
@@ -482,6 +494,7 @@ if __name__ == '__main__':
     parser.add_argument('--fold', type=int, default=-1)
     parser.add_argument('--weights', type=str, default='')
     parser.add_argument('--epoch', type=int, default=-1)
+    parser.add_argument('--apex', action='store_true')
 
     parser.add_argument('--resume_weights', type=str, default='')
     parser.add_argument('--resume_epoch', type=int, default=-1)
@@ -491,7 +504,7 @@ if __name__ == '__main__':
 
     if action == 'train':
         try:
-            train(model_name=args.model, run=args.run, fold=args.fold, resume_epoch=args.resume_epoch)
+            train(model_name=args.model, run=args.run, fold=args.fold, resume_epoch=args.resume_epoch, use_apex=args.apex)
         except KeyboardInterrupt:
             pass
 
