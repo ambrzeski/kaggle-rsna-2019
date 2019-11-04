01a087e53df5c853dfe46fe1784f3b6950efec8c
Tomasz Gilewicz <tomasz.gilewicz1@gmail.com>
2019-11-04 02:29:16 +0100

    Weighted sum of predictions

--- rsna19/models/smoothing/utils.py
+++ rsna19/models/smoothing/utils.py
@@ -3,13 +3,17 @@ from sklearn.metrics import log_loss
 from tqdm import tqdm
 
 
-def smooth_predictions(src, dst, std=0.7):
+def smooth_predictions(src, dst, std=0.7, show_progress=False):
     df = pd.read_csv(src)
     pred_columns = [c for c in df.columns if 'pred_' in c]
 
     new_df = df.copy()
 
-    for study_id in tqdm(df.study_id.unique()):
+    unique_ids = df.study_id.unique()
+    if show_progress:
+        unique_ids = tqdm(unique_ids)
+
+    for study_id in unique_ids:
         study_df = df[df.study_id == study_id].sort_values(by='slice_num')
         new_df.update(study_df[pred_columns].rolling(5, center=True, win_type='gaussian').mean(std=std))
 
@@ -45,3 +49,15 @@ def compute_log_loss_per_class(src):
         losses.append(log_loss(gt[i::6], pred[i::6]))
 
     return losses
+
+
+def test_log_loss_per_class():
+    from rsna19.configs.second_level import Config
+    avg_loss = compute_log_loss(Config().prediction_paths[0])
+    losses = compute_log_loss_per_class(Config().prediction_paths[0])
+    losses.append(losses[-1])
+    assert avg_loss == sum(losses) / len(losses)
+
+
+if __name__ == "__main__":
+    test_log_loss_per_class()
